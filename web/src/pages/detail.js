97be37c1d44eb9a5769e76b51cbb6b3781aeb446
Lylleny <514532528@qq.com>
2018-08-31 09:13:00 +0800

    add infomation

--- web/src/pages/detail.js
+++ web/src/pages/detail.js
@@ -7,8 +7,9 @@ class detail extends Component {
 
     render(){
         return(
-            <div style={{marginTop:70}}>
+            <div style={{marginTop:70,backgroundColor:'#F9F9F9'}}>
                 <text>this is my fist blog demo ,this is detail pages</text>
+                <text>add detail infomation from any apple computer</text>
             </div>
         )
     }
